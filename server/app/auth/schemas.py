e9dd9014797280d20ead1ebd3c604a4c153258b4
Suleiman Mai-Abba <suleyman.abba@gmail.com>
2025-11-30 20:33:06 +0100

    made refresh token optional in Token schema

--- server/app/auth/schemas.py
+++ server/app/auth/schemas.py
@@ -1,29 +1,29 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr
+from sqlmodel import SQLModel, EmailStr
 
 # TOKEN SCHEMAS
-class Token(BaseModel):
+class Token(SQLModel):
     access_token: str
     token_type: str = "bearer"
-    refresh_token: str
+    refresh_token: Optional[str] = None 
     expires_in: int
 
-class TokenPayload(BaseModel):
+class TokenPayload(SQLModel):
     sub: str | None = None
     type: str | None = None # "access" or "refresh"
     jti: str | None = None   # Unique Token Identifier for Blacklisting
 
 # LOGIN SCHEMAS
-class LoginRequest(BaseModel):
+class LoginRequest(SQLModel):
     email: EmailStr
     password: str
 
 # INVITE SCHEMAS
-class InviteCreate(BaseModel):
+class InviteCreate(SQLModel):
     max_uses: int = 1
     expires_in_days: Optional[int] = 7
 
-class InvitePublic(BaseModel):
+class InvitePublic(SQLModel):
     code: str
     uses_left: int
     expires_at: Optional[str]
