b73fcf7a6ae68c3d7e92c7ee13160bb0e206e157
Suleiman Mai-Abba <suleyman.abba@gmail.com>
2025-12-03 10:03:34 +0100

    added listing exceptions

---
+++ server/app/listings/exceptions.py
@@ -0,0 +1,13 @@
+class ListingException(Exception):
+    pass
+
+class InvalidCategoryData(ListingException):
+    """Raised when category specific fields are missing"""
+    pass
+
+# class TagNotAllowed(ListingException):
+#     """Raised when trying to use a tag that isn't official yet"""
+#     pass
+
+class ListingNotFound(ListingException):
+    pass
