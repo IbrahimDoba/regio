46a7c786bca800e332664e4d002d64c45ea04d1e
Suleiman Mai-Abba <suleyman.abba@gmail.com>
2025-12-03 10:00:50 +0100

    created banking module config

--- server/app/banking/config.py
+++ server/app/banking/config.py
@@ -2,6 +2,9 @@ from app.base_config import RegioBaseSettings
 
 class BankingConfig(RegioBaseSettings):
     SYSTEM_SINK_CODE: str
+    MONTHLY_FEE_MINUTES: int = 30
+    DEMURRAGE_THRESHOLD_MINUTES: int = 1800
+    DEMURRAGE_RATE_ANNUAL: float = 0.06
     
 
 banking_settings = BankingConfig()
